8b6fb3a74307cf02b2d306b105910a59784914f4
Joseph McGregor <joseph00@my.yorku.ca>
2022-02-22 09:48:07 -0500

    protect against injection attacks

--- bootcamp_app/teacher.js
+++ bootcamp_app/teacher.js
@@ -7,15 +7,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT teachers.id as teachers_id, teachers.name as name, cohorts.name as cohort
-FROM teachers
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+
+const queryString = `
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name LIKE $1 || 'JUL02'}'
+  ORDER BY teacher;
+  `;
+
+pool.query(queryString, values)
+
   .then(res => {
-    res.rows.forEach(user => {
-      console.log(`${user.name} has an id of ${user.teacher_id} and was in the ${user.cohort} cohort`);
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`);
     })
-  }).catch(err => console.error('query error', err.stack)); 
+  }).catch(err => console.error('query error', err.stack));
